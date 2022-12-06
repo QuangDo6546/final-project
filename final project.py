f0799530affc1d65b9829bf046a09cb4c22103d2
QuangDo6546 <qdo6546@conestogac.on.ca>
2022-12-06 16:02:39 -0500

    added a menu

--- final project.py
+++ final project.py
@@ -1,3 +1,16 @@
+import os
+
 fruits = ['Apple','Orange','Banana','Watermelon','Pine Apple','Coconut']
 
 result = ['Apple','Orange','Banana','Watermelon','Pine Apple','Coconut']
+
+def Menu():
+    os.system("cls")
+    i = 0
+    for option in fruits:
+        i = i + 1
+        print("{}. {}".format(i,option))
+
+
+
+Menu()
