80b38aa1639e0c6beb5e043f6579b02fbeab6841
QuangDo6546 <qdo6546@conestogac.on.ca>
2022-12-06 16:21:04 -0500

    added computer pick and os system

--- final project.py
+++ final project.py
@@ -1,8 +1,27 @@
 import os
-
+import random
 fruits = ['Apple','Orange','Banana','Watermelon','Pine Apple','Coconut']
 
 result = ['Apple','Orange','Banana','Watermelon','Pine Apple','Coconut']
+
+random.shuffle(result)
+
+picker = []
+player = []
+
+picker.append(result[0])
+picker.append(result[1])
+picker.append(result[2])
+
+
+def random_pick():
+    list_result = []
+    print("The result is: {}".format("".join(picker)))
+
+
+
+
+
 
 def Menu():
     os.system("cls")
@@ -11,6 +30,7 @@ def Menu():
         i = i + 1
         print("{}. {}".format(i,option))
 
-
-
+        choice = int(input("Enter a number of fruits you want to choice: "))
+        if (choice == 7):
+            print("Thanks for playing.")
 Menu()
