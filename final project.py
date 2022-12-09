e9d14cb6c8d597f34a7d870ef6724647624355b3
QuangDo6546 <qdo6546@conestogac.on.ca>
2022-12-08 21:06:44 -0500

    Added more inline comments. Assignment done

--- final project.py
+++ final project.py
@@ -43,31 +43,32 @@ list_result = []
 
 # Function to print the result
 def random_pick():
-    print("The result is: {}".format(", ".join(picker)))
+    print("The result is: {}".format(", ".join(picker)))    #Print the results
     for item in player:
         for up in picker:
             if(item==up):
                 list_result.append(up)      #Match the user input to result
+    #If user choice match any result, the program will print the fruits match, if not the program will print good luck next time
     if len(list_result)> 0:
         print("You won: {}".format(', '.join(list_result)))
-        print("Won fruits have been created to a text file at C: \ user \ your name \ fruits.txt")
+        print("Won fruits have been created to a text file at C: \ user \ your name \ fruits.txt")      #Print the message the fruits have been created to a text file
     else:
         print("Good luck next time! ")
 
-
+#Function to write the file
 def txt_file():
-    myFile = open("fruits.txt", "w")
+    myFile = open("fruits.txt", "w")    #open the file as write mode
     myFile.write("You won: ")
     i = 0
-    for data in list_result:
+    for data in list_result:            #Mke the list with number for every won fruits
         i = i + 1
         myFile.write("\n{}.{}".format(i,data))
    
-    myFile.close()
+    myFile.close()                      #Close the file
     
-
-
+#Function to display the list of fruits
 def Menu():
+    #Loop the list until the user does't want to choose anymore fruit
     while True:
         print("1.Apple")
         print("2.Orange")
@@ -77,39 +78,38 @@ def Menu():
         print("6.Coconut")
         print("7.Exit")
         
-
+        #Ask user enter a number of the fruits
         choice = int(input("Enter a number of fruits you want to choice: "))
-        if (choice == 7):
+        if (choice == 7):                       #If user type 7, the program will stop and display a message thanks for playing
             print("Thanks for playing.")
             return True
         
-        
-        if (choice == 0):
+        if (choice == 0):                       #If user type 0, the program will print the message ask user type other number
             print("Invalid input, try again!")
             print()
 
-        if (choice >=8):
+        if (choice >=8):                        #Same as > = 8
             print("Invalid input, try again!")        
         else:
             player.append(conever_choice(int(choice)-1))
             return False
 
+#Loop the input ask user if they want to choose more fruit or no, if not the program will stop and display the result
 while True:
-    
     if len(player)> 0:
             print()
             print()
-            print("You choose: {}".format(', '.join(player)))
-            continue_choice = input("Do you want to continue choice? (y/n) ")
-            if continue_choice == 'y':
+            print("You choose: {}".format(', '.join(player)))                 #Print the fruits user choose
+            continue_choice = input("Do you want to continue choice? (y/n) ") #Ask user if they want to choose more fruits
+            if continue_choice == 'y':                                         #If yes, the program will continue to display the menu
                 if Menu():
                     break
-            elif continue_choice == 'n':
+            elif continue_choice == 'n':                                       #If not, the program will stop and display the result
                 random_pick()
-                txt_file()
+                txt_file()                                                      #Call write function
                 break
-            else:
-                print("Please enter invalid input!")
+            else:                                                   
+                print("Please enter invalid input!")                            #If user type other than y/n, the program will continue to ask until they type y/n
     else:
         if Menu():
             break
